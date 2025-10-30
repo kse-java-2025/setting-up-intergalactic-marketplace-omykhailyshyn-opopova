289dd22487d2a1526e1e661f26115dfc373a5f6c
Olena Popova <opopova@kse.org.ua>
2025-10-31 01:03:35 +0200

    add Product domain model

---
+++ demo/src/main/java/com/example/demo/domain/Product.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain;
+
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true)
+public class Product {
+    UUID ProductId;
+    String ProductName;
+    Double ProductPrice;
+    String ProductCategory;
+}
