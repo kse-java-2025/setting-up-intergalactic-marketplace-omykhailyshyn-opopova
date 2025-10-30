593df2d34f49b6d0c20730a962d4415bca18a6df
Olena Popova <opopova@kse.org.ua>
2025-10-31 00:23:44 +0200

    replace DemoApplication with IntergalacticMarketplaceApp

---
+++ demo/src/main/java/com/example/demo/IntergalacticMarketplaceApp.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Primary;
+
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+public class IntergalacticMarketplaceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntergalacticMarketplaceApp.class, args);
+    }
+}
