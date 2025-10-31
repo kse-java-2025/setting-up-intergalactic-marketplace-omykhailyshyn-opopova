692fc654b906cacb386b8cfd1e77110b52f9031e
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2025-10-31 21:43:38 +0200

    fix: replace Card with Cart and add Order factory method

--- demo/src/main/java/com/example/demo/domain/Order.java
+++ demo/src/main/java/com/example/demo/domain/Order.java
@@ -1,11 +1,11 @@
 package com.example.demo.domain.order;
 
-import java.util.UUID;
+import com.example.demo.domain.product.Product;
+import com.example.demo.common.OrderStatus;
 import lombok.Builder;
 import lombok.Value;
 import java.util.List;
-import com.example.demo.common.OrderStatus;
-import com.example.demo.domain.product.Product;
+import java.util.UUID;
 
 @Value
 @Builder(toBuilder = true)
@@ -15,4 +15,16 @@ public class Order {
     Double totalPrice;        
     OrderStatus orderStatus;
     UUID customerId;
+
+    // factory method to create Order from Cart
+    // just use Order orderId is bad practice ?
+    public static Order createOrderFromCart(UUID customerId, List<Product> productEntries, Double totalPrice) {
+        return Order.builder()
+                .orderId(UUID.randomUUID())
+                .customerId(customerId)
+                .productEntries(productEntries)
+                .totalPrice(totalPrice)
+                .orderStatus(OrderStatus.PENDING)
+                .build();
+    }
 }
