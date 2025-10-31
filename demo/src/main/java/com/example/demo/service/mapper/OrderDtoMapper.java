2484d44e6c1e4460e8b1a68ad320017e33959b32
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2025-11-01 00:17:25 +0200

    feat: cart and order mappers

---
+++ demo/src/main/java/com/example/demo/service/mapper/OrderDtoMapper.java
@@ -0,0 +1,23 @@
+package com.example.demo.service.mapper;
+
+import com.example.demo.domain.Order;
+import com.example.demo.dto.order.OrderDto;
+import com.example.demo.dto.order.OrderEntryDto;
+import java.util.UUID;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface OrderDtoMapper {
+    @Mapping(target = "orderId", expression = "java(UUID.randomUUID())")
+    Order toOrder(OrderDto orderDto);
+
+    OrderEntryDto toOrderEntryDto(Order order); // correct??
+
+    default List<OrderEntryDto> toOrderEntryDtoList(List<Order> orders) { // correct??
+        return orders.stream()
+                .map(this::toOrderEntryDto)
+                .toList();
+    }
+}
